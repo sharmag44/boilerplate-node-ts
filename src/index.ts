5475b1d229eed4457cfd47c41bf3e28d6e7aaf29
sharmag44 <sharmagshambhu44@gmail.com>
2024-02-11 19:24:20 +0530

    type import in index file

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { userRoutes } from './routes/user.routes';
 import connectToDatabase from './config/mongoose.config';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import swaggerOptions from "./swaggerOptions"
+import swaggerOptions from './swaggerOptions';
+import { Request, Response } from 'express';
 // Define routes
 
 const specs = swaggerJsdoc(swaggerOptions);
@@ -11,14 +12,14 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api/users', userRoutes);
 
-app.get("/", async(req,res) =>{
-  return res.send({isSuccess:true,message:"Code Working"})
-})
+app.get('/', async (_req: Request, res: Response) => {
+      return res.send({ isSuccess: true, message: 'Code Working' });
+});
 
-connectToDatabase()
+connectToDatabase();
 // Start the server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
 });
